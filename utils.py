d584591d3a1562e5c1fc5cf5076ea54f4a210f62
solosolius <soliusaltijddemijne@gmail.com>
2022-07-30 10:51:42 +0800

    Added utils file that extracts the bus timing data

---
+++ utils.py
@@ -0,0 +1,9 @@
+#Util file that contains all the functions that extract the data from the API
+
+class busData:
+	def __init__(self,stopCode):
+		self.stopCode = stopCode
+
+	def getData(self):
+		
+
