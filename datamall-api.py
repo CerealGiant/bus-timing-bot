9f1b7a94a09284424bb018d36c886e4f8f5624e7
solosolius <soliusaltijddemijne@gmail.com>
2022-07-30 13:08:43 +0800

    Added file that interacts with datamall and gets the bus data

---
+++ datamall-api.py
@@ -0,0 +1,28 @@
+import json
+import requests
+
+URL = 'http://datamall2.mytransport.sg/'
+PATH = 'ltaodataservice/BusArrivalv2?'
+
+HEADERS = {
+'AccountKey': '/8ejWnEdS46+g+b5JC7+1w==',
+'accept': 'application/json'
+}
+
+def getData(busStopCode):
+	final_url = URL+PATH
+	parameters = {
+		'BusStopCode':busStopCode
+	}
+	
+	r = requests.get(final_url,headers=HEADERS,params=parameters)
+		
+	#Converting into a python dict
+	print(r.content)
+	file = json.loads(r.content)
+		
+	return file
+
+if __name__ == '__main__':
+	getData(83139)
+
